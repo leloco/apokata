77de678db66b54a94739f3548a32985f71bbcdc6
Leon Eifler <leoneifler@proton.me>
2026-02-04 23:06:12 +0100

    Refactor ansible structure and Add resource 'ops' to 'ninja'

--- variables.tf
+++ variables.tf
@@ -52,8 +52,21 @@ variable "ninja_tang_ipv4" {
   type        = string
   sensitive = true 
 }
+
 variable "tang_root_password" {
-  description = "The root password for the container tang"
+  description = "The root password for the lxc tang"
+  type        = string
+  sensitive = true 
+}
+
+variable "ninja_ops_ipv4" {
+  description = "The IPv4 with CIDR"
+  type        = string
+  sensitive = true 
+}
+
+variable "ops_root_password" {
+  description = "The root password for the lxc  ops"
   type        = string
   sensitive = true 
 }
