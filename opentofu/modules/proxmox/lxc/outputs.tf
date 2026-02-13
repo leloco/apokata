8a016d539b544269756174fbe32c76ec6e08ceab
Leon Eifler <leoneifler@proton.me>
2026-02-13 02:55:47 +0100

    Autocreate inventory.ini, Update ansible roles

--- opentofu/modules/proxmox/lxc/outputs.tf
+++ opentofu/modules/proxmox/lxc/outputs.tf
@@ -1,3 +1,10 @@
 output "vm_id" {
   value = tostring(proxmox_virtual_environment_container.lxc.vm_id)
 }
+output "hostname" {
+  value = proxmox_virtual_environment_container.lxc.initialization[0].hostname
+}
+
+output "ipv4_address" {
+  value = split("/", proxmox_virtual_environment_container.lxc.initialization[0].ip_config[0].ipv4[0].address)[0]
+}
