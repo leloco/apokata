212005253c2c3e61ff6eb4118ee5aa64594fd0aa
Leon Eifler <70845118+leloco@users.noreply.github.com>
2026-08-19 00:04:01 +0200

    Add ansible roles and tofu config for rocketchat (#121)

--- opentofu/infra/main.tf
+++ opentofu/infra/main.tf
@@ -112,6 +112,13 @@ locals {
        vm_id = 205
     }
 
+    z1_rocketchat = {
+       hostname = "z1-rocketchat"
+       ipv4_address = cidrhost(local.vlans.core.network, var.infra_z1_rocketchat_host_id)
+       ipv6_address = "${local.vlans.core.ula_prefix}${var.infra_z1_rocketchat_iid}"
+       vm_id = 206
+    }
+
 
     shadow = {
        hostname = "shadow"
@@ -165,6 +172,7 @@ ${local.mutable_hosts.prowl.hostname} ansible_host=${local.mutable_hosts.prowl.i
 ${local.mutable_hosts.unifi_controller.hostname} ansible_host=${local.mutable_hosts.unifi_controller.ipv4_address} ansible_host_ipv6=${local.mutable_hosts.unifi_controller.ipv6_address}
 ${local.mutable_hosts.z1_npm.hostname} ansible_host=${local.mutable_hosts.z1_npm.ipv4_address} ansible_host_ipv6=${local.mutable_hosts.z1_npm.ipv6_address}
 ${local.mutable_hosts.z1_portainer.hostname} ansible_host=${local.mutable_hosts.z1_portainer.ipv4_address} ansible_host_ipv6=${local.mutable_hosts.z1_portainer.ipv6_address}
+${local.mutable_hosts.z1_rocketchat.hostname} ansible_host=${local.mutable_hosts.z1_rocketchat.ipv4_address} ansible_host_ipv6=${local.mutable_hosts.z1_rocketchat.ipv6_address}
 
 [proxmox_vm]
 ${local.mutable_hosts.runner_alpha.hostname} ansible_host=${local.mutable_hosts.runner_alpha.ipv4_address}  ansible_user=${local.mutable_hosts.runner_alpha.user}
@@ -193,6 +201,7 @@ ${local.mutable_hosts.z1_portainer.hostname}
 
 [docker_agents_group]
 ${local.mutable_hosts.z1_npm.hostname}
+${local.mutable_hosts.z1_rocketchat.hostname}
 
 [docker_hosts_group:children]
 portainer_group
@@ -223,7 +232,7 @@ ansible_ssh_trusted_key_file=""
 ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 EOT
 
-depends_on = [ module.tang, module.prowl, module.unifi_controller, module.z1_npm, module.z1_portainer ]
+depends_on = [ module.tang, module.prowl, module.unifi_controller, module.z1_npm, module.z1_portainer, module.z1_rocketchat ]
 }
 
 resource "proxmox_virtual_environment_dns" "node_dns" {
@@ -384,6 +393,44 @@ module "z1_npm" {
       volume = "local-lvm:vm-${local.mutable_hosts.z1_npm.vm_id}-data"
       path   = "/opt/npm-data"
       size   = "1"
+      backup = true
+    }
+  ]
+}
+
+module "z1_rocketchat" {
+  source = "../modules/proxmox/lxc"
+
+  pve_node         = var.shared_pve_node
+  vm_id            = local.mutable_hosts.z1_rocketchat.vm_id
+  hostname         = local.mutable_hosts.z1_rocketchat.hostname
+  nameservers       = [local.virtual.ipv4_address, local.virtual.ipv6_address, local.vlans.core.gateway_ipv4, local.vlans.core.gateway_ipv6]
+  searchdomain     = var.shared_searchdomain
+
+  memory_dedicated = 4096
+  memory_swap      = 2048
+  cpu_cores        = 4
+
+  vlan_id          = local.vlans.core.id
+  template_file_id = var.shared_lxc_template_file_id
+
+  ipv4_address     = "${local.mutable_hosts.z1_rocketchat.ipv4_address}/24"
+  gateway          = local.vlans.core.gateway_ipv4
+
+  ipv6_address     = "${local.mutable_hosts.z1_rocketchat.ipv6_address}/64"
+  ipv6_gateway     = local.vlans.core.gateway_ipv6
+
+  ssh_public_key_file = var.shared_ssh_public_key_file
+
+  datastore_id     = var.shared_root_datastore_id
+  datastore_size   = "20"
+  start_on_boot    = var.shared_start_on_boot
+
+  mount_points = [
+    {
+      volume = "local-lvm:vm-${local.mutable_hosts.z1_rocketchat.vm_id}-data"
+      path   = "/opt/rocketchat-data"
+      size   = "10"
       backup = true
     }
   ]
